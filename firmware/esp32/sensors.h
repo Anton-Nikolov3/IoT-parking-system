6fbcd0fda25dc7b3712939e10de916db73981188
Anton-nikolov <100777577+Anton-Nikolov3@users.noreply.github.com>
2026-04-28 09:25:57 +0300

    Add sensor header with distance reading function

---
+++ firmware/esp32/sensors.h
@@ -0,0 +1,13 @@
+#define TRIG_PIN 5
+#define ECHO_PIN 18
+
+float readDistance() {
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+
+  long duration = pulseIn(ECHO_PIN, HIGH);
+  return duration * 0.034 / 2;
+}
